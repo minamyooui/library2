4eea6fbc037f5bfb941f11761d3227f98a038a8d
Marty <maruri2424@gmail.com>
2022-03-29 23:09:43 -0700

    styled cards and fixed form submit behavior

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const library = [];
 
-const addBook = document.querySelector('#addBook');
-addBook.addEventListener('click', openBookForm);
-
-for(let i = 0; i < 2; i++) {
+const openBookForm = document.querySelector('#openBookForm');
+openBookForm.addEventListener('click', openForm);
+const form = document.querySelector('form');
+form.addEventListener('submit', addBook);
+for(let i = 0; i < 3; i++) {
   const book = new Book('JoJo', 'JoJo', 447, true);
   addBookToLibrary(book);
 }
+
+displayBooks();
 
 function Book(title, author, pages, read) {
   this.title = title
@@ -19,7 +22,30 @@ function addBookToLibrary(book) {
   library.push(book)
 }
 
+function addBook(e) {
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const read = getReadValue();
+  const book = new Book(title, author, pages, read);
+  addBookToLibrary(book);
+  displayBooks();
+  e.preventDefault();
+}
+
+function getReadValue() {
+  let radios = document.getElementsByName('read');
+  for (let i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      // do whatever you want with the checked radio
+      return radios[i].id == 'yes' ? true : false;
+      // only one radio can be logically checked, don't check the rest
+    }
+  }
+}
+
 function displayBooks() {
+  clearDisplay();
   const cards =  document.querySelector('.cards');
   library.forEach(book => {
     const card = createCard(book);
@@ -27,21 +53,32 @@ function displayBooks() {
   });
 }
 
-const createCard(book) {
+function  clearDisplay() {
+  const cards =  document.querySelector('.cards');
+  while(cards.firstChild) {
+    cards.removeChild(cards.firstChild);
+  }
+}
+
+function createCard(book) {
   const card = document.createElement('div');
   card.classList.add('card');
   const title = document.createElement('p');
   title.textContent = 'Title: ' + book.title;
   const author = document.createElement('p');
-  author.textContent = 'Author ' + book.author;
+  author.textContent = 'Author: ' + book.author;
   const pages = document.createElement('p');
   pages.textContent = 'Pages: '+ book.pages;
   const read = document.createElement('p');
   read.textContent = book.read ? 'Read' : 'Not Read';
-  card.appendChild
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(read);
+  return card;
 }
 
-function openBookForm() {
+function openForm() {
   const form = document.querySelector('.form');
   form.classList.toggle('hideform');
 }
